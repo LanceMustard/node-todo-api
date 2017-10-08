8d24f7b83a6fad15b48ee8680bfb3fe2413aa2e2
Lance Mustard <Lance.Mustard@hotmail.com>
2017-10-08 15:25:46 +0800

    leason 92

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 // const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var saltvalue = '65782352';
 var data = {
   id: 4
 };
+var password = '123abc!';
 
-var token = jwt.sign(data, saltvalue);
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var decoded = jwt.verify(token +'1', saltvalue);
-console.log('decoded' + JSON.stringify(decoded));
+var hashedPassword = '$2a$10$0DSmLduxZgGzHQprAYgs7uiTFOenw/R4MxEM47w1DfG.xu.LRj9qO';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var token = jwt.sign(data, saltvalue);
+// console.log(token);
+
+// var decoded = jwt.verify(token +'1', saltvalue);
+// console.log('decoded' + JSON.stringify(decoded));
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 
 var UserSchema = new mongoose.Schema({
@@ -59,17 +60,29 @@ UserSchema.statics.findByToken = function(token) {
     decoded = jwt.verify(token, 'abc123');
   } catch (err) {
     return Promise.reject();
-    // return new Promise((resolve, reject) => {
-    //   reject();
-    // });
   }
   return User.findOne({
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
   });
-  
 };
+
+// hash user password
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
